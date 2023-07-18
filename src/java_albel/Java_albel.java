f021d606a1c62a186b3eaf419df09fa8695ea57c
AlbelArva <117788921+AlbelArva@users.noreply.github.com>
2023-07-18 09:32:40 +0700

    Login Multi User

---
+++ src/java_albel/Java_albel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_albel;
+
+/**
+ *
+ * @author Albel Arva
+ */
+public class Java_albel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
